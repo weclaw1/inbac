8ce3bce517727b46c92612a4bb8c7e58cbd0982c
Robert Węcławski <r.weclawski@gmail.com>
2019-10-24 08:44:25 +0200

    basic functionality works

--- inbac/inbac.py
+++ inbac/inbac.py
@@ -18,22 +18,22 @@ aspect_ratio_y = 1
 
 selection_box_color = "black"
 
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        
         self.pack(fill=tk.BOTH, expand=tk.YES)
-        if len(sys.argv) > 1:
-            self.input_dir = sys.argv[1]
-        else:
-            self.input_dir = os.getcwd()
-        self.files = self.load_images(self.input_dir)
-        self.output_dir = os.path.join(self.input_dir, output_directory)
 
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
+        if len(sys.argv) > 1:
+            self.input_directory = sys.argv[1]
+        else:
+            self.input_directory = os.getcwd()
+
+        self.files = self.load_images(self.input_directory)
+
+        output_directory_path = os.path.join(self.input_directory, output_directory)
+        if not os.path.exists(output_directory_path):
+            os.makedirs(output_directory_path)
 
         self.selection_box = None
 
@@ -43,6 +43,8 @@ class Application(tk.Frame):
         self.image_canvas = tk.Canvas(self, highlightthickness=0)
         self.image_canvas.pack(fill=tk.BOTH, expand=tk.YES)
 
+        self.canvas_image = None
+
         self.master.bind('z', self.save_next)
         self.master.bind('x', self.save)
         self.master.bind('<Left>', self.previous_image)
@@ -51,49 +53,63 @@ class Application(tk.Frame):
         self.master.bind('<B1-Motion>', self.on_mouse_drag)
 
         self.current_file = 0
+        
+        self.master.geometry("800x600")
+        self.master.update()
+        self.load_image_from_file(self.files[self.current_file])
 
-        self.load_image_from_file(self.files[self.current_file], True)
+    def load_image_from_file(self, filename):
+        self.clear_canvas(self.image_canvas)
+        
+        self.original_image = Image.open(os.path.join(self.input_directory, filename))
+        self.width = self.original_image.size[0]
+        self.height = self.original_image.size[1]
+        if self.width > self.master.winfo_width() or self.height > self.master.winfo_height():
+            width_ratio = float(self.master.winfo_width()) / self.width
+            height_ratio = float(self.master.winfo_height()) / self.height
+            ratio = min(width_ratio, height_ratio)
+            self.width = int(float(self.width) * float(ratio))
+            self.height = int(float(self.height) * float(ratio))
 
-    def load_image_from_file(self, filename, first_image):
-        self.image_canvas.delete("all")
-        self.current_filename = filename
-        self.original_image = Image.open(os.path.join(self.input_dir, filename))
-        if first_image:
-            self.width = 800
-        else:
-            self.width = self.master.winfo_width()
-        width_percent = self.width/float(self.original_image.size[0])
-        self.height = int(float(self.original_image.size[1]) * float(width_percent))
-        self.master.geometry(str(self.width) + "x" + str(self.height))
         self.displayed_image = self.original_image.copy()
-        self.displayed_image.thumbnail((self.width, self.height), Image.NEAREST)
+        self.displayed_image.thumbnail((self.width, self.height), Image.ANTIALIAS)
         self.displayed_image = ImageTk.PhotoImage(self.displayed_image)
-        self.image_canvas.create_image(0, 0, anchor=tk.NW, image=self.displayed_image)
+        self.canvas_image = self.image_canvas.create_image(0, 0, anchor=tk.NW, image=self.displayed_image)
+
+    def clear_canvas(self, widget):
+        self.clear_selection_box(widget)
+        if self.canvas_image is not None:
+            widget.delete(self.canvas_image)
+            self.canvas_image = None
 
     def save_next(self, event=None):
         self.save()
         self.next_image()
 
     def save(self, event=None):
-        box = self.get_selected_box()
-        new_filename = self.find_available_name(self.output_dir, self.current_filename)
-        subprocess.run(
-            ["convert", os.path.join(self.input_dir, self.current_filename),
-             "-crop", str(box[2] - box[0]) + "x" + str(box[3] - box[1]) + "+" + str(box[0]) + "+" + str(box[1]),
-             "-resize", str(resize_width) + "x" + str(resize_height),
-             ">", os.path.join(self.output_dir, new_filename)], shell=True)
+        if self.selection_box is None:
+            return
+        filename = self.files[self.current_file]
+        output_directory_path = os.path.join(self.input_directory, output_directory)
+        box = self.get_real_box(self.get_selected_box())
+        new_filename = self.find_available_name(output_directory_path, filename)
+        subprocess.run(["convert", os.path.join(self.input_directory, filename),
+                        "-crop", str(box[2] - box[0]) + "x" + str(box[3] - box[1]) + "+" + str(box[0]) + "+" + str(box[1]),
+                        "-resize", str(resize_width) + "x" + str(resize_height),
+                        os.path.join(output_directory_path, new_filename)])
+        self.clear_selection_box(self.image_canvas)
 
     def next_image(self, event=None):
         if self.current_file + 1 >= len(self.files):
             return
         self.current_file += 1
-        self.load_image_from_file(self.files[self.current_file], False)
+        self.load_image_from_file(self.files[self.current_file])
 
     def previous_image(self, event=None):
         if self.current_file - 1 < 0:
             return
         self.current_file -= 1
-        self.load_image_from_file(self.files[self.current_file], False)
+        self.load_image_from_file(self.files[self.current_file])
 
     def on_mouse_down(self, event):
         self.mouse_press_coord = (event.x, event.y)
@@ -117,10 +133,10 @@ class Application(tk.Frame):
             widget.coords(self.selection_box, selected_box)
         
     def get_real_box(self, selected_box):
-        return (selected_box[0] * self.original_image.size[0]/self.displayed_image.width(),
-                selected_box[1] * self.original_image.size[1]/self.displayed_image.height(),
-                selected_box[2] * self.original_image.size[0]/self.displayed_image.width(),
-                selected_box[3] * self.original_image.size[1]/self.displayed_image.height())
+        return (int(selected_box[0] * self.original_image.size[0]/self.displayed_image.width()),
+                int(selected_box[1] * self.original_image.size[1]/self.displayed_image.height()),
+                int(selected_box[2] * self.original_image.size[0]/self.displayed_image.width()),
+                int(selected_box[3] * self.original_image.size[1]/self.displayed_image.height()))
 
     def get_selected_box(self):
         selection_top_left_x = min(self.mouse_press_coord[0], self.mouse_move_coord[0])
@@ -135,41 +151,46 @@ class Application(tk.Frame):
             aspect_ratio = float(aspect_ratio_x)/float(aspect_ratio_y)
             selection_box = self.get_selection_box_for_aspect_ratio(selection_box, aspect_ratio)
         
-        return tuple(lambda x: int(round(x)) for x in selection_box)
+        return tuple((lambda x: int(round(x)))(x) for x in selection_box)
 
     def get_selection_box_for_aspect_ratio(self, selection_box, aspect_ratio):
+        selection_box = list(selection_box)
         width = selection_box[2] - selection_box[0]
         height = selection_box[3] - selection_box[1]
-        if float(width)/float(height) > aspect_ratio:
-            height = width / aspect_ratio
-            if self.mouse_move_coord[1] > self.mouse_press_coord[1]:
-                selection_box[4] = selection_box[1] + height
+        try:
+            if float(width)/float(height) > aspect_ratio:
+                height = width / aspect_ratio
+                if self.mouse_move_coord[1] > self.mouse_press_coord[1]:
+                    selection_box[3] = selection_box[1] + height
+                else:
+                    selection_box[1] = selection_box[3] - height
             else:
-                selection_box[1] = selection_box[4] - height
-        else:
-            width = height * aspect_ratio
-            if self.mouse_move_coord[0] > self.mouse_press_coord[0]:
-                selection_box[2] = selection_box[0] + width
-            else:
-                selection_box[0] = selection_box[2] - width
-        return selection_box
+                width = height * aspect_ratio
+                if self.mouse_move_coord[0] > self.mouse_press_coord[0]:
+                    selection_box[2] = selection_box[0] + width
+                else:
+                    selection_box[0] = selection_box[2] - width
+        except ZeroDivisionError:
+            pass
+        return tuple(selection_box)
 
     @staticmethod
-    def load_images(dir):
+    def load_images(directory):
         all_images = []
-        for filename in os.listdir(dir):
+        for filename in os.listdir(directory):
             _, extension = os.path.splitext(filename)
             if extension.lower() not in image_extensions: continue
             all_images.append(filename)
         return all_images
 
     @staticmethod
-    def find_available_name(dir, filename):
-        if os.path.isfile(os.path.join(dir, filename)):
+    def find_available_name(directory, filename):
+        name, extension = os.path.splitext(filename)
+        if not os.path.isfile(os.path.join(directory, filename)):
             return filename
         for num in itertools.count(2):
-            if os.path.isfile(os.path.join(dir, filename + str(num))):
-                return filename + str(num)
+            if not os.path.isfile(os.path.join(directory, name + str(num) + extension)):
+                return name + str(num) + extension
 
 
 root = tk.Tk()
