4dc0660e6a5dd047a0c8b6a88dfc35f885e575d7
Robert Węcławski <r.weclawski@gmail.com>
2019-11-22 12:09:21 +0100

    fix existing tests

--- tests/test_inbac.py
+++ tests/test_inbac.py
@@ -3,6 +3,7 @@ import unittest
 import unittest.mock as mock
 
 from inbac.inbac import Application
+from inbac.controller import Controller
 
 
 class TestInbac(unittest.TestCase):
@@ -13,7 +14,7 @@ class TestInbac(unittest.TestCase):
         filename = "test.jpg"
         mock_path_isfile.return_value = False
 
-        returned_filename = Application.find_available_name(
+        returned_filename = Controller.find_available_name(
             directory, filename)
         mock_path_isfile.assert_called_with(os.path.join(directory, filename))
         self.assertEqual(filename, returned_filename)
@@ -25,7 +26,7 @@ class TestInbac(unittest.TestCase):
         new_filename = "test2.jpg"
         mock_path_isfile.side_effect = file_exist
 
-        returned_filename = Application.find_available_name(
+        returned_filename = Controller.find_available_name(
             directory, filename)
         calls = [mock.call(os.path.join(directory, filename)),
                  mock.call(os.path.join(directory, new_filename))]
@@ -38,7 +39,7 @@ class TestInbac(unittest.TestCase):
         mouse_move_coord = (15.0, 9.0)
         selection_box = (0.0, 0.0, 15.0, 9.0)
         expected_selection_box = (0.0, 0.0, 16.0, 9.0)
-        returned_selection_box = Application.get_selection_box_for_aspect_ratio(selection_box, aspect_ratio,
+        returned_selection_box = Controller.get_selection_box_for_aspect_ratio(selection_box, aspect_ratio,
                                                                                 mouse_press_coord, mouse_move_coord)
         self.assertEqual(expected_selection_box, returned_selection_box)
 
@@ -46,7 +47,7 @@ class TestInbac(unittest.TestCase):
         mouse_press_coord = (0.0, 0.0)
         mouse_move_coord = (15.0, 9.0)
         expected_selection_box = (0.0, 0.0, 15.0, 9.0)
-        returned_selection_box = Application.get_selected_box(
+        returned_selection_box = Controller.get_selected_box(
             mouse_press_coord, mouse_move_coord, None)
         self.assertEqual(expected_selection_box, returned_selection_box)
 
@@ -54,28 +55,28 @@ class TestInbac(unittest.TestCase):
         mouse_press_coord = (15.0, 9.0)
         mouse_move_coord = (0.0, 0.0)
         expected_selection_box = (0.0, 0.0, 15.0, 9.0)
-        returned_selection_box = Application.get_selected_box(
+        returned_selection_box = Controller.get_selected_box(
             mouse_press_coord, mouse_move_coord, None)
         self.assertEqual(expected_selection_box, returned_selection_box)
 
     def test_get_real_box(self):
         selected_box = (2, 2, 4, 4)
         expected_real_box = (4, 4, 8, 8)
-        returned_real_box = Application.get_real_box(
+        returned_real_box = Controller.get_real_box(
             selected_box, (10, 10), (5, 5))
         self.assertEqual(expected_real_box, returned_real_box)
 
     @mock.patch('inbac.inbac.os.listdir')
     def test_load_images_with_wrong_filetype(self, mock_listdir):
         mock_listdir.return_value = ["test.txt", "test2"]
-        returned_images = Application.load_image_list("/home/test/")
+        returned_images = Controller.load_image_list("/home/test/")
         self.assertListEqual([], returned_images)
 
     @mock.patch('inbac.inbac.os.listdir')
     def test_load_images(self, mock_listdir):
         mock_listdir.return_value = ["test.txt", "test2.jpg"]
         directory = "/home/test/"
-        returned_images = Application.load_image_list(directory)
+        returned_images = Controller.load_image_list(directory)
         self.assertListEqual(["test2.jpg"], returned_images)
 
 
